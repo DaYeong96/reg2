5996f08a963eb8932aa4c534157233963c1578ab
DaYeong96 <102666062+DaYeong96@users.noreply.github.com>
2022-06-09 15:35:00 +0900

    Update app2.py

--- app2.py
+++ app2.py
@@ -59,7 +59,7 @@ model_call = joblib.load("model.pkl")
 new_x_df = user_input_features()
 
 st.write(new_x_df) 
-st.write(new_x_df['station'].dtype) 
+#st.write(new_x_df['station'].dtype) 
 
 
 from sklearn.preprocessing import OneHotEncoder
@@ -68,10 +68,8 @@ ohe = OneHotEncoder(sparse=False)
 data_cat2 = ohe.fit_transform(new_x_df[['station']])
 
 
-
-
 # data_cat2 = ohe_station.transform(new_x_df[['station']])
-# data_concat = pd.concat([new_x_df.drop(columns=['station']),pd.DataFrame(data_cat2, columns=['station_' + str(col) for col in ohe_station.categories_[0]])], axis=1)
+data_concat = pd.concat([new_x_df.drop(columns=['station']),pd.DataFrame(data_cat2, columns=['station_' + str(col) for col in ohe_station.categories_[0]])], axis=1)
 
 data_con_scale = scaler_call.transform(data_concat)
 result = model_call.predict(data_con_scale) 
